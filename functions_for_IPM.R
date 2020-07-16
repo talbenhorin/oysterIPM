c566dcd522c099dbad19155bb27fcaf23ba4ce39
Tal Ben-Horin <talbenhorin@Tals-MacBook-Air.local>
2020-07-16 15:17:37 -0400

    update

--- functions_for_IPM.R
+++ functions_for_IPM.R
@@ -226,3 +226,348 @@ get_vm_ratio = function(P, y){
   
 }
 
+g0_x = function(x, params){
+  # Define the initial infection function
+  return(dnorm(x, mean=params$clump_mean, sd=params$clump_sd))
+  
+}
+
+s_x = function(x, params) {
+  
+  # Define survival function
+  u = exp(params$surv_int + params$surv_size * x)
+  return(u / (1 + u))
+  
+}
+
+
+r_x = function(x, params) {
+  
+  # Define the loss function, r(x) (also defined as l(x))
+  
+  u = exp(params$loss_int + params$loss_size*x + params$loss_temp)
+  probs = u / (1 + u)
+  
+  # Truncate loss probability. Above this load no loss can occur
+  probs[x > params$loss_trunc] = 0
+  return(probs)
+  
+}
+
+
+k_xpx = function(xp, x, params){
+  
+  # Define the full kernel (for eviction calculations)
+  
+  return(g_xpx(xp, x, params) * s_x(x, params) * (1 - r_x(x, params)))
+  
+}
+
+
+g_xpx = function(xp, x, params){
+  
+  # Tranistion from x to xp given some parameters
+  
+  if(params$linear){ # Linear growth function
+    
+    return(dnorm(xp, mean=params$growth_int + params$growth_size * x +
+                   params$growth_temp,
+                 sd=sqrt(params$growth_sigma2*exp(2 * params$growth_sigma2_exp * x))))
+    
+  } else{ # Non-linear growth function
+    
+    x[x <= params$growth_deriv0] = params$growth_deriv0
+    return(dnorm(xp, mean=params$growth_int + params$growth_size * x +
+                   params$growth_temp + params$growth_size_sq * x**2,
+                 sd=sqrt(params$growth_sigma2*exp(2 * params$growth_sigma2_exp * x))))
+  }
+}
+
+
+set_temp_params = function(desired_temp, params, linear){
+  
+  # Specific parameters for each temperature, given a desired temp
+  #
+  # Parameters
+  # ----------
+  # desired_temp: int, the desired temperature at which to calculate params
+  # params: list, a list-like object with the base params
+  # linear: bool, TRUE is you are using a linear growth function. FALSE otherwise
+  #
+  # Returns
+  # --------
+  # : Updated parameters
+  
+  tparams = data.frame(holder=NA)
+  
+  # Two parameters for survival fxn, same for everybody
+  tparams$surv_int = params$surv_int
+  tparams$surv_size = params$surv_slope
+  
+  if(linear){ # Assuming a linear effect of size
+    
+    # 5 parameters for growth function
+    tparams$growth_int = params$growth_int
+    tparams$growth_temp = params$growth_temp * desired_temp
+    tparams$growth_size = params$growth_size
+    tparams$growth_sigma2 = params$growth_sigma2
+    tparams$growth_sigma2_exp = params$growth_sigma2_exp
+    
+    # Three parameters for the loss function
+    tparams$loss_int = params$loss_int
+    tparams$loss_size = params$loss_size
+    tparams$loss_temp = params$loss_temp * desired_temp
+    
+    # Probability of becoming infected
+    u = exp(params$prob_inf_int + params$prob_inf_temp * desired_temp)
+    tparams$prob_inf = u / (1 + u)
+    
+    # Clumped distribution
+    tparams$clump_mean = params$clump_int + params$clump_temp * desired_temp
+    tparams$clump_sd = sqrt(params$clump_sigma2*exp(2 * params$clump_sigma2_exp * desired_temp))
+    
+    # Class zero survival
+    tparams$class_zero_surv = 1
+    tparams$linear = TRUE
+    
+  } else{ # Assuming a non-linear effect of size
+    
+    # Parameters for growth function
+    tparams$growth_int = params$growth_int
+    tparams$growth_temp = params$growth_temp * desired_temp
+    tparams$growth_size = params$growth_size + params$growth_size_temp_int * desired_temp
+    tparams$growth_size_sq = params$growth_size_sq
+    tparams$growth_sigma2 = params$growth_sigma2
+    tparams$growth_sigma2_exp = params$growth_sigma2_exp
+    
+    # Find where the derivative is 0
+    tparams$growth_deriv0 = (-1 * tparams$growth_size) / (2 * tparams$growth_size_sq)
+    
+    # Three parameters for the loss function
+    tparams$loss_int = params$loss_int
+    tparams$loss_size = params$loss_size
+    tparams$loss_temp = params$loss_temp * desired_temp
+    
+    # Probability of becoming infected
+    u = exp(params$prob_inf_int + params$prob_inf_temp * desired_temp)
+    tparams$prob_inf = u / (1 + u)
+    
+    # Clumped distribution
+    tparams$clump_mean = params$clump_int + params$clump_temp * desired_temp
+    tparams$clump_sd = sqrt(params$clump_sigma2*exp(2 * params$clump_sigma2_exp * desired_temp))
+    
+    # Class zero survival
+    tparams$class_zero_surv = 1
+    tparams$linear = FALSE
+    
+  }
+  
+  return(tparams)
+}
+
+
+### Various transmission functions ###
+
+density_dependent_zoospores = function(n_t, beta, params){
+  
+  # Function specifying the probability of infection given the total
+  # number zoospores in the population
+  #
+  # Parameters
+  # n_t : Population vector
+  # beta : transmission coefficient
+  # y : midpoints for IPM (different IPM loads)
+  # params : IPM parameters
+  
+  # Parameter specifying baseline probability of infection
+  a = -1 * log(1 - params$prob_inf)
+  
+  # This is funky...need to count total zoospores...un-log transform and then
+  # re-transform + 1...what effect is this having?
+  
+  total_zoospores_t = log(sum(exp(y) * n_t[2:(bins + 1)]) + 1)
+  
+  # Calculate the infection probability
+  infection_prob = 1 - exp(-(a + params$beta * total_zoospores_t))
+  
+  return(infection_prob)
+}
+
+density_dependent_individuals = function(n_t, y, params){
+  
+  # Function specifying the probability of infection given the total
+  # number zoospores in the population
+  #
+  # Parameters
+  # n_t : Population vector
+  # beta : transmission coefficient
+  # params : IPM parameters
+  
+  # Parameter specifying baseline probability of infection
+  a = -1 * log(1 - params$prob_inf)
+  
+  total_inf_individuals_t = sum(n_t[2:(bins + 1)])
+  
+  # Calculate the infection probability
+  infection_prob = 1 - exp(-(a + params$beta * total_inf_individuals_t))
+  
+  return(infection_prob)
+}
+
+###########################################################################
+
+within_season_simulation = function(n_0, params, bins, bnd, y, h, kernel){
+  
+  # Calculates a one step projection given a vector n_0
+  #
+  # Parameters
+  # ----------
+  # n_0 : initial population vector
+  # params : parameters dataframe
+  # bins, bnd, y, h : discretization parameters
+  # kernel : density independent IPM kernel
+  
+  # Returns
+  # -------
+  # list of updated pop vector and infection probability
+  
+  n_t = as.matrix(n_0, nrow=bins + 1, ncol=1)
+  
+  # Calculate the time variant portion of the kernel. This will be updated
+  # each time step based on the distribution of zoospores.  Initiall assuming
+  # density dependent transmission. The total number of zoospores across all
+  # frogs.
+  
+  infection_prob = density_dependent_zoospores(n_t, y, params)
+  
+  # The zero -> zero transition
+  zero_trans = params$class_zero_surv * (1 - infection_prob)
+  
+  # The zero to nonzero transitions: Transitioning from zero to infected
+  prob_clump = h * dnorm(y, mean=params$clump_mean, sd=params$clump_sd)
+  ztnz = params$class_zero_surv * infection_prob * prob_clump
+  col1 = c(zero_trans, ztnz)
+  
+  # Compute the infected to uninfected row. This is just the probability of
+  # surviving with load $x$ ($s(x)$) and then losing an infection $r(x)$.  We
+  # just need to multiply $s(x) r(x)$
+  
+  # The loss probabilities: Transitioning from non-zero to zero
+  nztz = kernels$S * kernels$R
+  row1 = c(zero_trans, nztz)
+  
+  # Now we just need to stick these extra columns onto the **P** matrix. The
+  # most likely thing that is going to happen is you are going to stay in the
+  # uninfected class.
+  
+  full_P = get_full_P(P, row1, col1, min_size, y, plot_it=F)
+  
+  n_t_new = full_P %*% n_t
+  
+  return(list(nt=n_t_new, inf_prob=infection_prob))
+  
+}
+
+###########################################################################
+
+build_full_P = function(min_size, max_size, bins, params) {
+  
+  # Function that builds the full transition matrix for the host parasite
+  # IPM
+  
+  # Parameters
+  # min_size: float, lower bound of the matrix
+  # max_size: float, upper bound of the matrix
+  # bins: int, number of bins in the matrix
+  # params: list, list of parameters to use to build P
+  
+  # Returns
+  # full_P : full transition matrix
+  
+  # Get the relevant matrix parameters: midpoints, cell edges, and cell width
+  matrix_params = set_discretized_values(min_size, max_size, bins)
+  bnd = matrix_params$bnd
+  y = matrix_params$y
+  h = matrix_params$h
+  
+  # Calculate and plot eviction.  Eviction is most important when e is large and. Using the script provided by Williams et al. for checked for eviction
+  
+  # Get the kernel of the IPM without the zero class
+  kernels = get_the_kernel(g_xpx, s_x, r_x, bins, y, params, h, plot_it=F)
+  P = kernels$P
+  
+  # The zero -> zero transition
+  zero_trans = params$class_zero_surv * (1 - params$prob_inf)
+  
+  # The zero to nonzero transitions: Transitioning to zero to infected
+  prob_clump = h * dnorm(y, mean=params$clump_mean, sd=params$clump_sd)
+  ztnz = params$class_zero_surv * params$prob_inf * prob_clump
+  col1 = c(zero_trans, ztnz)
+  
+  # The loss probabilities: Transitioning from non-zero to zero
+  nztz = kernels$S * kernels$R
+  row1 = c(zero_trans, nztz)
+  
+  # Now we just need to stick these extra columns onto the **P** matrix. The
+  # most likely thing that is going to happen is you are going to stay in the
+  # uninfected class.
+  
+  full_P = get_full_P(P, row1, col1, min_size, y, plot_it=F)
+  
+  return(full_P)
+  
+}
+
+###########################################################################
+
+monte_carlo_params = function(params){
+  # For the various vital functions in the IPM, uses the vcov matrix for each
+  # set of params to draw new params (assuming multivariate normality under
+  # asymptotic likelihood theory) and return a params matrix with the updated
+  # params
+  
+  # Parameters
+  # params : list, properly formatted list of parameters from ipm_vital*.R
+  
+  # Returns
+  # rand_params : list, list where growth, loss, surv, clump, and prob_inf
+  # parameters are random draws
+  
+  rand_params = params
+  vital_names = c('growth', 'loss', 'surv', 'clump', 'prob_inf')
+  
+  for(vital_name in vital_names){
+    
+    # Draw params for each vital function
+    mean_vect = paste(vital_name, "coef", sep="_")
+    vcov_mat = paste(vital_name, "vcov", sep="_")
+    #print(paste(mean_vect, vcov_mat))
+    vital_draw = mvrnorm(n=1, mu=params[[mean_vect]], Sigma=params[[vcov_mat]])
+    vital_draw = as.list(vital_draw)
+    
+    # Store params if they exist
+    rand_params[[paste(vital_name, "int", sep="_")]] = vital_draw[["(Intercept)"]]
+    rand_params[[paste(vital_name, "temp", sep="_")]] = vital_draw[["temp"]]
+    rand_params[[paste(vital_name, "size", sep="_")]] = vital_draw[["size"]]
+    rand_params[[paste(vital_name, "size_sq", sep="_")]] = vital_draw[["I(size^2)"]]
+    rand_params[[paste(vital_name, "size_temp_int", sep="_")]] = vital_draw[["size:temp"]]
+    
+  }
+  
+  return(rand_params)
+  
+}
+
+################################
+
+compute_quartiles = function(est_matrix){
+  # Computes the median, lower quartile, and upper quartile of est_matrix
+  # column wise
+  
+  median = apply(est_matrix, 2, median)
+  upper = apply(est_matrix, 2, uq)
+  lower = apply(est_matrix, 2, lq)
+  
+  return(list(lower=lower, median=median, upper=upper))
+  
+}
